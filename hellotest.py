dd792eef4005f88a18885d705065acdcecd8338b
suthisuk158 <79188367+suthisuk158@users.noreply.github.com>
2021-04-07 15:26:48 +0700

    Update hellotest.py

--- hellotest.py
+++ hellotest.py
@@ -3,16 +3,10 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template("home.html")
+    return "6006021612158 สุธิศกัดิ์ พรมอ่อน"
 
-@app.route("/John")
-def John():
-    return "Hello John."
 
-@app.route('/aboutUs')
-def aboutUs():
-    return render_template("aboutUs.html")
 
 if __name__ == '__main__':
     app.debug = True
-    app.run(host='0.0.0.0', port=8000)
+    app.run(host='0.0.0.0', port=80)
